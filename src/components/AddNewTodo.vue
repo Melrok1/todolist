b45f8025cc8500c4022ba0115c705cbb0eb3b55a
Melrok <martin.blascak86@gmail.com>
2023-11-10 08:15:36 +0100

    init

---
+++ src/components/AddNewTodo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="add-new-todo-wrapper">
+    <input type="text" name="todoTitle" />
+    <input type="text" name="todoContent" />
+    <date-picker />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import DatePicker from '../components/InputComponents/DatePicker.vue'
+
+export default defineComponent({
+  name: 'AddNewTodoForm',
+  components: {
+    DatePicker
+  },
+});
+</script>
---
+++ src/components/InputComponents/DatePicker.vue
@@ -0,0 +1,21 @@
+<template>
+  <VueDatePicker v-model="date"></VueDatePicker>
+</template>
+
+<script lang="ts">
+import { ref } from 'vue';
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
+
+export default {
+	name: 'DatePickerComponent',
+  components: { VueDatePicker },
+  setup() {
+		const date = ref<Date | null>(null);
+
+    return {
+      date
+    }
+  }
+};
+</script>

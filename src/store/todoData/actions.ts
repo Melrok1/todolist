f84ed6fb8266d975151c9d1704c5965456990788
Melrok <martin.blascak86@gmail.com>
2023-11-16 10:07:33 +0100

    modified structure for todo data - vuex

--- src/store/todoData/actions.ts
+++ src/store/todoData/actions.ts
@@ -1,39 +1,28 @@
 import { ActionTree } from "vuex";
 import { TodoItemType, TodoStateType } from "@/ts/interface";
-import {
-  getFirestore,
-  collection,
-  getDocs,
-  doc,
-  setDoc,
-  deleteDoc,
-} from "firebase/firestore";
+import { getFirestore, collection, getDocs, doc, setDoc, deleteDoc } from "firebase/firestore";
 
 const actions: ActionTree<TodoStateType, object> = {
+  //ADD NEW TODO
   async addTodo({ commit }, todoItem: TodoItemType) {
     const db = getFirestore();
     const todoId = todoItem.id.toString();
     const todoRef = doc(db, "todos", todoId);
-
     await setDoc(todoRef, todoItem);
 
     commit("ADD_TODO", { ...todoItem, id: todoRef.id });
   },
 
-  async toggleTodoDone(
-    { commit },
-    payload: { isDoneState: boolean; todoId: string }
-  ) {
+  // TOGGLE IS DONE STATE
+  async toggleTodoDone({ commit }, payload: { isDoneState: boolean; todoId: string }) {
     const db = getFirestore();
     const todoRef = doc(db, "todos", payload.todoId.toString());
-
-    // Aktualizujeme Todo vo Firestore
     await setDoc(todoRef, { isDone: payload.isDoneState }, { merge: true });
 
-    // Aktualizujeme Todo vo Vuexu
     commit("TOGGLE_TODO_DONE", payload);
   },
 
+  //DELETE TODO
   async deleteTodo({ commit }, todoId: number) {
     const db = getFirestore();
     const todoRef = doc(db, "todos", todoId.toString());
@@ -42,17 +31,16 @@ const actions: ActionTree<TodoStateType, object> = {
     commit("DELETE_TODO", todoId);
   },
 
-  async updateTodo(
-    { commit },
-    payload: { updatedTodo: TodoItemType; todoId: string }
-  ) {
+  //UPDATE TODO
+  async updateTodo({ commit }, payload: { updatedTodo: TodoItemType; todoId: string }) {
     const db = getFirestore();
     const todoRef = doc(db, "todos", payload.todoId.toString());
-
     await setDoc(todoRef, payload.updatedTodo, { merge: true });
+
     commit("UPDATE_TODO", payload);
   },
 
+  // LOAD TODOS FROM FIRESTORE
   async fetchTodos({ commit }) {
     const db = getFirestore();
     const todoCollection = collection(db, "todos");
@@ -64,6 +52,7 @@ const actions: ActionTree<TodoStateType, object> = {
         dueDate: data.dueDate?.toDate(),
       };
     });
+
     commit("SET_TODOS", todos);
   },
 };
